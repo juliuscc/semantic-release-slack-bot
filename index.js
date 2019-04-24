aa82207eceb399577d696338429baf1d761742ac
Hannes Rabo <hannes.rabo@gmail.com>
2019-04-24 11:21:17 +0200

    fix: Added verifyCondition hook to check for the correct environment variable
    #1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const verifyConditions = require('./lib/verifyConditions.js')
 const success = require('./lib/success')
 const fail = require('./lib/fail')
 
 module.exports = {
+	verifyConditions,
 	success,
 	fail
 }
