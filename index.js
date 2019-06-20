1c52b72856dffc9d61cbe6e971b27aa5597617af
jcelik <julius.cc@hotmail.com>
2019-06-20 21:36:26 +0200

    fix: obvious fail

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const verifyConditions = require('./lib/verifyConditions.js')
 const success = require('./lib/success')
-const fail = require('./lib/fail')
+const fail = require('./lib/fail');
 
 module.exports = {
 	verifyConditions,
