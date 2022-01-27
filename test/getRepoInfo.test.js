ef6a1c2d81a64fa42275b8b3db2417a7ac00684c
Florian Migot <florian.migot@quicksign.com>
2022-01-27 23:20:40 +0100

    fix: Add compatibility for gitlab tag url in default success template

--- test/getRepoInfo.test.js
+++ test/getRepoInfo.test.js
@@ -1,11 +1,12 @@
 const assert = require('assert')
 const getRepoInfo = require('../lib/getRepoInfo')
 
-const runAssert = (repoUrl, path, url) => {
+const runAssert = (repoUrl, path, url, hostname) => {
   const actual = getRepoInfo(repoUrl)
 
   assert.equal(path, actual.path)
   assert.equal(url, actual.URL)
+  assert.equal(hostname, actual.hostname)
 }
 
 describe('test getRepoInfo', () => {
@@ -13,41 +14,47 @@ describe('test getRepoInfo', () => {
     const repositoryUrl = 'ssh://git@github.com:hello/world.git'
     const expectedPath = 'hello/world'
     const expectedUrl = 'https://github.com/hello/world'
-    runAssert(repositoryUrl, expectedPath, expectedUrl)
+    const expectedHostname = 'github.com'
+    runAssert(repositoryUrl, expectedPath, expectedUrl, expectedHostname)
   })
 
   it('should work for bitbucket', () => {
     const repositoryUrl = 'ssh://hg@bitbucket.org/hello/world.git'
     const expectedPath = 'hello/world'
     const expectedUrl = 'https://bitbucket.org/hello/world'
-    runAssert(repositoryUrl, expectedPath, expectedUrl)
+    const expectedHostname = 'bitbucket.org'
+    runAssert(repositoryUrl, expectedPath, expectedUrl, expectedHostname)
   })
 
   it('should work for gitlab', () => {
     const repositoryUrl = 'ssh://git@gitlab.com:hello/world.git'
     const expectedPath = 'hello/world'
     const expectedUrl = 'https://gitlab.com/hello/world'
-    runAssert(repositoryUrl, expectedPath, expectedUrl)
+    const expectedHostname = 'gitlab.com'
+    runAssert(repositoryUrl, expectedPath, expectedUrl, expectedHostname)
   })
 
   it('should work for repo url with https', () => {
     const repositoryUrl = 'https://github.com/hello/world.git'
     const expectedPath = 'hello/world'
     const expectedUrl = 'https://github.com/hello/world'
-    runAssert(repositoryUrl, expectedPath, expectedUrl)
+    const expectedHostname = 'github.com'
+    runAssert(repositoryUrl, expectedPath, expectedUrl, expectedHostname)
   })
 
   it('should work for repo url with git@', () => {
     const repositoryUrl = 'git@github.com:hello/world.git'
     const expectedPath = 'hello/world'
     const expectedUrl = 'https://github.com/hello/world'
-    runAssert(repositoryUrl, expectedPath, expectedUrl)
+    const expectedHostname = 'github.com'
+    runAssert(repositoryUrl, expectedPath, expectedUrl, expectedHostname)
   })
 
   it('should work for repo url with other TLD', () => {
     const repositoryUrl = 'git@github.pl:hello/world.git'
     const expectedPath = 'hello/world'
     const expectedUrl = 'https://github.pl/hello/world'
-    runAssert(repositoryUrl, expectedPath, expectedUrl)
+    const expectedHostname = 'github.pl'
+    runAssert(repositoryUrl, expectedPath, expectedUrl, expectedHostname)
   })
 })
